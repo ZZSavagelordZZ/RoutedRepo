d1483eed0ca6503b6d6fc081fe22574dfd1227da
ZZSavagelordZZ <khashab2004@gmail.com>
2024-09-10 21:18:34 +0300

    Goldmember

--- app/Golden/page.jsx
+++ app/Golden/page.jsx
@@ -1,5 +1,4 @@
 import { Golden_Landing } from "@/components/component/golden_landing";
-import Test from "@/components/component/test";
 
 export default function cardetails() {
   return <Golden_Landing/>
